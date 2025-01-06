4936f871f4bb79a58914041e4d7bb3856c302e12
Laxman Rathod <laxmanrathod.dev@gmail.com>
2025-01-06 20:49:06 +0530

    feat(auth): implement clerk setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,16 @@
 import type { Metadata } from "next"
 import { Plus_Jakarta_Sans } from "next/font/google"
-import { ReduxProvider } from "@/redux/provider"
+import { ClerkProvider } from "@clerk/nextjs"
+// import { ReduxProvider } from "@/redux/provider"
+import { ReactQueryProvider } from "@/react-query/provider"
 import "./globals.css"
 
 const jakarta = Plus_Jakarta_Sans({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Grouple",
-  description: "Grouple LMS",
+  description:
+    "Grouple is a cutting-edge learning management system that revolutionizes online education. With advanced features for course creation, student engagement, and progress tracking, it empowers educators and learners alike. Experience seamless collaboration, interactive content delivery, and data-driven insights in one comprehensive SaaS platform.",
 }
 
 export default function RootLayout({
@@ -16,10 +19,16 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={`${jakarta.className} bg-black`}>
-        <ReduxProvider>{children}</ReduxProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${jakarta.className} bg-black`}>
+          {/* <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange> */}
+          {/* <ReduxProvider> */}
+          <ReactQueryProvider>{children}</ReactQueryProvider>
+          {/* </ReduxProvider> */}
+          {/* </ThemeProvider> */}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
